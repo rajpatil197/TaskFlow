4296eb2b67da53d6ae778db966305bdf76eae970
rajpatil197 <patilrajvardhan420@gmail.com>
2026-08-10 22:48:52 +0530

    Api developed

--- taskflow-backend/taskflow-backend/src/main/java/com/taskflow/controller/UserController.java
+++ taskflow-backend/taskflow-backend/src/main/java/com/taskflow/controller/UserController.java
@@ -1,7 +1,12 @@
 package com.taskflow.controller;
 
 
+import com.taskflow.dto.UserCreateRequest;
+import com.taskflow.dto.UserResponse;
+import com.taskflow.entity.User;
 import com.taskflow.service.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +18,12 @@ public class UserController {
 
     public UserController(UserService userService) {
         this.userService = userService;
+    }
+
+
+    @PostMapping
+    public UserResponse createUser(@RequestBody UserCreateRequest userRequest){
+        return userService.createUser(userRequest);
     }
 
 }
